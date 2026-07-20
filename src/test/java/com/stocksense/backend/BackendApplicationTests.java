f7b71b9e8eab1c871b9cb7e9afcc6bcf7bd24cbd
Santhosh <santhoshsg362@gmail.com>
2026-07-21 04:57:35 +0530

    Phase 1 complete: Spring Boot project connected to MySQL

---
+++ src/test/java/com/stocksense/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocksense.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
